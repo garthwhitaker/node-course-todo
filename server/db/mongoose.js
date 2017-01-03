a543ce1bc474726b302ec64e63a645f978aae7c8
gwhitaker <garth.whitaker@metropolitan.co.za>
2017-01-03 08:57:02 +0200

    Change mongodb connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-// mongoose.connect('mongodb://garth:password@ds133438.mlab.com:33438/todoapplication');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://garth:password@ds133438.mlab.com:33438/todoapplication');
 
 module.exports = {
     mongoose
