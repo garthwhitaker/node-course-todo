70e186ea28faddc6121f575c06d89ea75dc265b8
gwhitaker <garth.whitaker@metropolitan.co.za>
2017-01-04 14:38:02 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -154,11 +154,29 @@ app.post('/users', (req, res) => {
         });
 });
 
-
+//GET /users/me
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
+//POST /users/login
+app.post('/users/login', (req, res) => {
+
+    var body = _.pick(req.body, ['email', 'password']);
+    User.findByCredentials(body.email, body.password)
+        .then((user) => {
+            return user.generateAuthToken()
+                .then((token) => {
+                    res.header('x-auth', token).send(user);
+                })
+        
+        })
+        .catch((err) => {
+            res.status(400);
+            res.send();
+        });
+
+});
 
 
 app.listen(port, () => {
