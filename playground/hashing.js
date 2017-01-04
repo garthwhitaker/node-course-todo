024e65512fe2502b1cf5ee1e49dc4987c23f7c23
gwhitaker <garth.whitaker@metropolitan.co.za>
2017-01-04 09:53:40 +0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    data: 44
-};
+var password = '123abc!';
+// var salt = bcrypt.genSalt(10, (err, salt) => {
 
-var token = jwt.sign(data,'123abc');
-console.log(token);
+//     bcrypt.hash(password, salt,(error,hash)=>{
+//         console.log(hash)
+//     });
+// });
 
 
-var  decoded= jwt.verify(token,'123abc');
-console.log(decoded);
+var hashedPassword = '$2a$10$tPWRHbF3RLfvpi8OUWW3LuINJmH5hnu3iU1IspsS6eI3UY5hyrtEG';
+bcrypt.compare(password, hashedPassword, (error, result)=>{
+
+console.log(result);
+});
+// var data = {
+//     data: 44
+// };
+
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+
+
+// var  decoded= jwt.verify(token,'123abc');
+// console.log(decoded);
 // var message = 'Tom tom';
 // console.log(`plain: ${message}, hash: ${SHA256(message).toString()}`);
 
